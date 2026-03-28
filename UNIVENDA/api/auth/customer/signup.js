31e58088924a0edcbeab9358f513fb15e7eaa792
Ashutosh Giri <ashugoswami339@gmail.com>
2026-03-29 01:20:24 +0530

    fix on 29 march 2026

--- UNIVENDA/api/auth/customer/signup.js
+++
@@ -1,7 +0,0 @@
-import { createSignupHandler } from "../../_lib/auth-handlers.js";
-import { USER_ROLES } from "../../_lib/auth.js";
-
-export default createSignupHandler({
-  role: USER_ROLES.CUSTOMER,
-  capabilities: [],
-});
--- UNIVENDA/api/auth/seller/login.js
+++
@@ -1,6 +0,0 @@
-import { createLoginHandler } from "../../_lib/auth-handlers.js";
-import { USER_ROLES } from "../../_lib/auth.js";
-
-export default createLoginHandler({
-  role: USER_ROLES.SELLER,
-});
